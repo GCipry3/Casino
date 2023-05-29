31c717b5feb20f36aa11b43c78d1fdeb9e4ec226
claudiuohr <claudiu-constantin.ohriniuc@student.tuiasi.ro>
2023-05-29 18:01:30 +0300

    Change slots tests

--- CasinoTests/HigherLowerGameTests/HigherLowerTests.cs
+++ CasinoTests/HigherLowerGameTests/HigherLowerTests.cs
@@ -23,68 +23,54 @@ namespace CasinoTests.higherLowerTests
         [TestMethod]
         public void TestCalculateWinnings()
         {
-            // Arrange
             higherLower.BetValue = 10;
             higherLower.Option = "higher";
             string firstImg = "_02trefla";
             string secondImg = "_03trefla";
 
-            // Act
             int winnings = higherLower.CalculateWinnings(firstImg, secondImg);
 
-            // Assert
             Assert.AreEqual(50, winnings);
         }
 
         [TestMethod]
         public void TestCalculateWinnings_NoWin()
         {
-            // Arrange
             higherLower.BetValue = 10;
             higherLower.Option = "higher";
             string firstImg = "_05trefla";
             string secondImg = "_04trefla";
 
-            // Act
             int winnings = higherLower.CalculateWinnings(firstImg, secondImg);
 
-            // Assert
             Assert.AreEqual(0, winnings);
         }
 
         [TestMethod]
         public void TestRemoveCard_NotExistingCard()
         {
-            // Arrange
             string imgToRemove = "_21trefla";
 
-            // Act
             higherLower.RemoveCard(imgToRemove);
 
-            // Assert
             Assert.AreEqual(52, higherLower.Images.Count);
         }
 
         [TestMethod]
         public void TestRemoveCard()
         {
-            // Arrange
             string imgToRemove = "_02trefla";
 
-            // Act
             higherLower.RemoveCard(imgToRemove);
 
-            // Assert
             Assert.IsFalse(higherLower.Images.ContainsKey(imgToRemove));
         }
 
         [TestMethod]
         public void TestGetRandomImage()
         {
-            // Act
             KeyValuePair<string, System.Drawing.Image> randomImage = higherLower.GetRandomImage();
 
-            // Assert
             Assert.IsNotNull(randomImage);
             Assert.IsTrue(higherLower.Images.ContainsValue(randomImage.Value));
         }
